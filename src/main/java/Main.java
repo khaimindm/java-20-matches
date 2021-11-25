509eed5cacd009f1b1ebc668e11826590459cfbf
Dmitrii Khaimin <khaimindm@gmail.com>
2021-11-25 16:00:33 +0400

    Start of design 25.11.2021

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import service.GameMessages;
+
+public class Main {
+
+    public static void main(String[] args) {
+        GameMessages messages = new GameMessages();
+        System.out.println(messages.test("D", "K"));
+
+    }
+
+}
---
+++ src/main/java/service/GameMessages.java
@@ -0,0 +1,8 @@
+package service;
+
+public class GameMessages {
+
+    public String test(String name, String lastName){
+        return name + lastName;
+    }
+}
